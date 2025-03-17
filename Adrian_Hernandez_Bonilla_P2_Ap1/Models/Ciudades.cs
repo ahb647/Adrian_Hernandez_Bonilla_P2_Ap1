c57e8bd4e9d36c69e6e4fa5789dcc7d2544bccc3
Adrian Hernández Bonilla <adrian_hernandez@ucne.edu.do>
2025-03-17 18:43:03 -0400

    se eliminaron espacios innecesarios

--- Adrian_Hernandez_Bonilla_P2_Ap1/Models/Ciudades.cs
+++ Adrian_Hernandez_Bonilla_P2_Ap1/Models/Ciudades.cs
@@ -6,7 +6,6 @@ namespace Adrian_Hernandez_Bonilla_P2_Ap1.Models
     {
         [Key]
         public int CiudadId { get; set; }
-       
         public string NombreCiudad { get; set; } 
         public double Monto { get; set; }   
 
